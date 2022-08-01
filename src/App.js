823fb6d6991a2182fc922165e9aba622201a3653
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-01 10:09:38 -0400

    Add config of the project in Firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAKsvCMyRoCeEUVOHJFzWTrOikmL4eU-ig",
+  authDomain: "superchat-3e163.firebaseapp.com",
+  projectId: "superchat-3e163",
+  storageBucket: "superchat-3e163.appspot.com",
+  messagingSenderId: "330282569535",
+  appId: "1:330282569535:web:6451cdc77752774e5b0d8a",
+  measurementId: "G-Q077ZDZEK4"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
