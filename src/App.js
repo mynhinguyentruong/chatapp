6afabce4e2b6aa507734ca1f1a587af54089987a
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-01 10:12:40 -0400

    useAuthState hook and pass auth as argument and assign the value to user

--- src/App.js
+++ src/App.js
@@ -23,11 +23,17 @@ const auth = firebase.auth()
 const firestore = firebase.firestore()
 
 function App() {
+
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
       <header className="App-header">
         
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
